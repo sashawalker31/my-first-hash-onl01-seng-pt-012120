ec91258adde798cfc66c48db156493126d0f3c89
sasha walker <sashawalker31@gmail.com>
2020-02-01 14:19:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -43,9 +43,9 @@ def adding
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"]=4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
